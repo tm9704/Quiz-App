cbd71f7c2cd0243025bb137294a2bbf1818ab0cd
tm9704 <tm9704@korea.ac.kr>
2022-09-06 14:01:34 +0900

    first commit

---
+++ app/src/main/java/com/tm9704/myquizapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tm9704.myquizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
