b85bf1ad2ca695478d2700cdfb15a0bb6e59d713
tm9704 <tm9704@korea.ac.kr>
2022-09-13 21:32:43 +0900

    Quiz App: Choich Answer & Go to Next Question

--- app/src/main/java/com/tm9704/myquizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/tm9704/myquizapp/QuizQuestionsActivity.kt
@@ -4,14 +4,9 @@ import android.graphics.Color
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.TextView
+import android.widget.*
 import androidx.core.content.ContextCompat
-import org.w3c.dom.Text
 
 class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -59,8 +54,7 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun setQuestion() {
-
-        mCurrentPosition = 1
+        defaultOptionsView()
         val question: Question = mQuestionsList!![mCurrentPosition - 1]
         ivImage?.setImageResource(question.image)
         progressBar?.progress = mCurrentPosition
@@ -76,6 +70,7 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         }else{
             btnSubmit?.text = "SUBMIT"
         }
+
     }
 
     private fun defaultOptionsView(){
@@ -97,7 +92,7 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
             option.setTextColor(Color.parseColor("#7A8089"))
             option.typeface = Typeface.DEFAULT
             option.background = ContextCompat.getDrawable(
-                this,
+                this@QuizQuestionsActivity,
                 R.drawable.default_option_border_bg
             )
         }
@@ -105,7 +100,6 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun selectedOptionView(tv:TextView, selectedOptionNum: Int){
         defaultOptionsView()
-
         mSelectedOptionPosition = selectedOptionNum
 
         tv.setTextColor(Color.parseColor("#363A43"))
@@ -130,19 +124,78 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
             }
 
             R.id.tv_option_three -> {
-                tvOptionOne?.let {
+                tvOptionThree?.let {
                     selectedOptionView(it, 3)
                 }
             }
 
             R.id.tv_option_four -> {
-                tvOptionOne?.let {
+                tvOptionFour?.let {
                     selectedOptionView(it, 4)
                 }
             }
 
             R.id.btn_submit -> {
-                //TODO "implement btn submit"
+                if(mSelectedOptionPosition == 0){
+
+                    mCurrentPosition++
+
+                    when{
+                        mCurrentPosition <= mQuestionsList!!.size ->{
+                            setQuestion()
+                        }
+                        else -> {
+                            Toast.makeText(this,"You made it to the end", Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }else{
+                    val question = mQuestionsList?.get(mCurrentPosition - 1)
+
+                    if(question!!.correctAnswer != mSelectedOptionPosition){
+                        answerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
+                    }
+                    answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
+
+                    if(mCurrentPosition == mQuestionsList!!.size){
+                        btnSubmit?.text="FINISH"
+                    }else{
+                        btnSubmit?.text="GO TO NEXT QUESTION"
+                    }
+
+                    mSelectedOptionPosition = 0
+                }
+            }
+        }
+    }
+
+    private fun answerView(answer: Int, drawableView: Int){
+        when(answer){
+            1-> {
+                tvOptionOne?.background = ContextCompat.getDrawable(
+                    this@QuizQuestionsActivity,
+                    drawableView
+                )
+            }
+
+            2-> {
+                tvOptionTwo?.background = ContextCompat.getDrawable(
+                    this@QuizQuestionsActivity,
+                    drawableView
+                )
+            }
+
+            3-> {
+                tvOptionThree?.background = ContextCompat.getDrawable(
+                    this@QuizQuestionsActivity,
+                    drawableView
+                )
+            }
+
+            4-> {
+                tvOptionFour?.background = ContextCompat.getDrawable(
+                    this@QuizQuestionsActivity,
+                    drawableView
+                )
             }
         }
     }
